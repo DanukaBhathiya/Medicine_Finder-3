774a0154e093c8a208d975d7645b1fff618afad3
DanukaBhathiya <danukabatiya@gmail.com>
2022-09-26 15:43:30 +0530

    email verification done

--- Medicine-Finder-3/src/main/java/com/example/demo/security/config/WebSecurityConfig.java
+++ Medicine-Finder-3/src/main/java/com/example/demo/security/config/WebSecurityConfig.java
@@ -1,6 +1,6 @@
 package com.example.demo.security.config;
 
-import com.example.demo.appuser.AppUserService;
+import com.example.demo.service.AppUserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
